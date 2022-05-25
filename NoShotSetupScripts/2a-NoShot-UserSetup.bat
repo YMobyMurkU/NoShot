cdcff3a996b758bfd6f81334a7c0c59fd8ccfd3b
YMobyMurkU <105930326+YMobyMurkU@users.noreply.github.com>
2022-05-25 15:47:40 -0500

    Add 2a-NoShot-UserSetup.bat via upload

---
+++ NoShotSetupScripts/2a-NoShot-UserSetup.bat
@@ -0,0 +1,1 @@
+powershell.exe /c clear;Write-Host 'Creating Python Virtual Env.';py -m venv ..\NoShotVirtEnv;..\NoShotVirtEnv\Scripts\activate.bat;Write-Host 'Updating Virtual Envs Pip and SetupTools Packages';py -m pip install --upgrade pip setuptools;Write-Host 'Installing Packages to Virtual Env';py -m pip install requests tailer Pillow PyQt5 PyQt5-sip PyQt5-stubs pytesseract argparse
